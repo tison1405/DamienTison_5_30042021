f3bb78f17ae46dfbdccd0f95cfb1cb52cdf92e0a
tison1405 <77247038+tison1405@users.noreply.github.com>
2021-05-06 13:55:29 +0200

    création listing sur index

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 let listing = document.getElementById("listing");
-function nameTeddy(element1){
+// la fonction teddy crée une div de la boucle avec un Id nameTeddy//
+ function teddy(element1){
+  var x = document.createElement("div");
+  x.setAttribute('id', element1);
+  listing.appendChild(x); 
+}
+//la fonction nameTeddy crée un paragraphe dans la div contenant le nom//
+ function nameTeddy(element1){
     var x = document.createElement("P");
     var t = document.createTextNode(element1);
     x.appendChild(t);
-    listing.appendChild(x);
+    x.classList.add("nameTeddy");
+    document.getElementById(element1).appendChild(x);
 }
-
-function imageTeddy(element1) {
+//la fonction imageTeddy importe l'image du teddy dans la div// 
+ function imageTeddy(element1, element2) {
     var x = document.createElement("IMG");
-    x.setAttribute("src", element1);
+    x.setAttribute("src", element2);
     x.setAttribute("width", "304");
     x.setAttribute("height", "228");
-    listing.appendChild(x);
+    x.classList.add("imageTeddy");
+    document.getElementById(element1).appendChild(x);
   }
 
 
-fetch("http://localhost:3000/api/teddies/5be9c8541c9d440000665243")
+fetch("http://localhost:3000/api/teddies/")
   .then(function(res) {
     if (res.ok) {
       return res.json();
@@ -23,14 +32,23 @@ fetch("http://localhost:3000/api/teddies/5be9c8541c9d440000665243")
   })
 
   .then(function(value) {
-    console.log(value);
-    nameTeddy (value.name);
-    imageTeddy (value.imageUrl); 
+    {
+      let x;
+        //la boucle extrait les valeurs des attributs "name" et "image" de chaque objet du tableau "value"//
+        for (x of value){
+          teddy(x.name);
+          nameTeddy(x.name);
+          imageTeddy(x.name, x.imageUrl)
+
+        };
+
+    }
   })
 
   .catch(function(err) {
     // Une erreur est survenue
   });
+  
       
   
   
